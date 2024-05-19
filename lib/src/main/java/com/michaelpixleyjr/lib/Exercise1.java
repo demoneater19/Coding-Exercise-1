d2fa943afdb88980698648f4f1cc4fc6289704ea
demoneater19 <85873875+demoneater19@users.noreply.github.com>
2024-05-18 20:31:26 -0400

    Coding Exercise 1

---
+++ lib/src/main/java/com/michaelpixleyjr/lib/Exercise1.java
@@ -0,0 +1,24 @@
+package com.michaelpixleyjr.lib;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args){
+
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println("Enter the First Number: ");
+        int n1 = s1.nextInt();
+
+        System.out.println("Enter the Second Number: ");
+        int n2 = s1.nextInt();
+
+        float d = (float) n1 / n2;
+        float r = n1 % n2;
+
+        System.out.println("The division result is: " + d);
+        System.out.println("n1 / n2 = " + d);
+        System.out.println("The remainder = " +r);
+    }
+}
